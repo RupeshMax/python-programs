01b4bd1d24a0db1000687156e89eb4f011b04b70
RupeshMax <46364757+RupeshMax@users.noreply.github.com>
2019-12-05 16:31:04 +0530

    Add files via upload

---
+++ quiz.py
@@ -0,0 +1,10 @@
+"************* \n Welcome to Python Quiz*************"
+x=int(input("Enter 1 to start a quix"))
+que=['Author of Python','Which function is used to display a user content','which function is used to get data from user']
+option=[['gvr','sj','james'],['print','input','format','eval'],['print','input','format','eval']]
+ans=[{'q1':'gvr'},{'q2':'print'},{'q3':'input'}]
+if x==1:
+    print("your Question {}".format())
+    print("Description Author of Python\n")
+
+    option=[]
