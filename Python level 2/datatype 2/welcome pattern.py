741dda14942bb8f9bcbe62d170978d7c4d7ba9a7
RupeshMax <rupesh.maxpain@gmail.com>
2020-03-08 13:02:23 +0530

    datatype

--- Python level 2/datatype 2/welcome pattern.py
+++ Python level 2/datatype 2/welcome pattern.py
@@ -4,16 +4,19 @@ y=int(n[1])
 z=(y//2)-1
 for i in range(1,x*2,2):
     if i<x:
-        print('-'*(z)+('.|.'*i)+('-'*(z)))
+        #print('-'*(z)+('.|.'*i)+('-'*(z)))
+        print(('.|.'*a).center(y,'-'))
         z-=3
         a=i
     elif i>x:
         z+=3
-        print('-'*(z)+('.|.'*a)+('-'*(z)))
+        #print('-'*(z)+('.|.'*a)+('-'*(z)))
+        print(('.|.'*a).center(y,'-'))
         a-=2
         #print(i)
     else:
-        print('-'*((y-7)//2)+'WELCOME'+'-'*((y-7)//2))
+        #print('-'*((y-7)//2)+'WELCOME'+'-'*((y-7)//2))
+        print("WELCOME".center(y,'-'))
 '''
 ==================== Testcase 1 ===================
 3 9
